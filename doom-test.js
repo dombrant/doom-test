fface409c6e1ac99ca2d4abe91c8d432a5011acc
Dominic Brant <dombrant@gmail.com>
2022-05-03 15:19:50 -0600

    added rounding for values in MB

--- doom-test.js
+++ doom-test.js
@@ -9,9 +9,9 @@ try {
   // Convert bundle size from bytes to megabytes
   const bundleSize = (await getDirectorySize(projectPath)) / 1024 ** 2;
   if (bundleSize < 2.39) {
-    console.log(`${chalk.green("Build passed")}: ${bundleSize}MB`);
+    console.log(`${chalk.green("Build passed")}: ${bundleSize.toFixed(2)}MB`);
   } else {
-    console.log(`${chalk.red("Build failed")}: ${bundleSize}MB`);
+    console.log(`${chalk.red("Build failed")}: ${bundleSize.toFixed(2)}MB`);
   }
 } catch (error) {
   console.error(`${chalk.red("Promise failed")}: ${error}`);
