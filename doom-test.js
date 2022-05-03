834a590513ea068ab8651d24768d407da99c692a
Dominic Brant <dombrant@gmail.com>
2022-05-03 15:45:02 -0600

    added pretty-bytes

--- doom-test.js
+++ doom-test.js
@@ -1,17 +1,18 @@
 import { getDirectorySize } from "./lib/getDirectorySize.js";
 import logSymbols from "log-symbols";
 import chalk from "chalk";
+import prettyBytes from "pretty-bytes";
 
 const args = process.argv.slice(2);
 const projectPath = args[0] || "dist";
 
 try {
-  // Convert bundle size from bytes to megabytes
-  const bundleSize = (await getDirectorySize(projectPath)) / 1024 ** 2;
-  if (bundleSize < 2.39) {
-    console.log(`${chalk.green("Build passed")}: ${bundleSize.toFixed(2)}MB`);
+  const bundleSize = await getDirectorySize(projectPath);
+  // Use`1024 ** 2 to convert the value from bytes to megabytes`;
+  if (bundleSize / 1024 ** 2 < 2.39) {
+    console.log(`${chalk.green("Build passed")}: ${prettyBytes(bundleSize)}`);
   } else {
-    console.log(`${chalk.red("Build failed")}: ${bundleSize.toFixed(2)}MB`);
+    console.log(`${chalk.red("Build failed")}: ${prettyBytes(bundleSize)}`);
   }
 } catch (error) {
   console.error(`${chalk.red("Promise failed")}: ${error}`);
